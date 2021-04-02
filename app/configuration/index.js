29d150179d8eda99db3a97a6622a6376473b12da
pmerlo <p.merlofeter@gmail.com>
2021-04-02 11:17:02 +0200

    use env variable to load app config and validate with schema

--- app/configuration/index.js
+++ app/configuration/index.js
@@ -1,5 +1,24 @@
-const config = require("./dev.json");
+const process = require("process");
+const Validator = require("jsonschema").Validator;
+
+const schema = require("./config.schema.json");
+
+const loadConfig = function () {
+  switch (process.env.NODE_ENV) {
+    case "dev":
+      return require("./files/dev.json");
+    default:
+      throw Error(`invalid environment '${process.env.NODE_ENV}'`);
+  }
+};
+
+const validate = function (config) {
+  const result = new Validator().validate(config, schema);
+  if (result.errors.length > 0) throw Error(result.errors[0].stack);
+};
 
 module.exports = function (app) {
+  const config = loadConfig();
+  validate(config);
   app.config = config;
 };
